7ca3246e18b56d6e2a401c9af51771bdda5fc955
gidon.bauer <g.bauer@itv.rwth-aachen.de>
2025-07-10 19:27:53 +0200

    Play around with TwoPhaseSolver

--- src/TwoPhaseSolver.cpp
+++ src/TwoPhaseSolver.cpp
@@ -21,8 +21,8 @@
 // = Config ========================================================================================
 using Float = double;
 
-constexpr Index NX = 5 * 64;
-constexpr Index NY = 64;
+constexpr Index NX = 5 * 128;
+constexpr Index NY = 128;
 
 constexpr Float X_MIN = 0.0;
 constexpr Float X_MAX = 5.0;
@@ -30,18 +30,26 @@ constexpr Float Y_MIN = 0.0;
 constexpr Float Y_MAX = 1.0;
 
 constexpr Float T_END    = 5.0;
-constexpr Float DT_MAX   = 1e-1;
-constexpr Float CFL_MAX  = 0.9;
-constexpr Float DT_WRITE = 5e-2;
+constexpr Float DT_MAX   = 1e-4;
+constexpr Float CFL_MAX  = 0.5;
+constexpr Float DT_WRITE = 1e-2;
 
 constexpr Float U_BCOND = 1.0;
-constexpr Float U_0     = 1.0;
+constexpr Float U_0     = 0.0;
 constexpr Float VISC_G  = 1e-3;
-constexpr Float RHO_G   = 0.9;
+constexpr Float RHO_G   = 1.0;
 constexpr Float VISC_L  = 1e-1;
-constexpr Float RHO_L   = 1.0;
+constexpr Float RHO_L   = 10.0;
 
-constexpr int PRESSURE_MAX_ITER = 500;
+constexpr Float SURFACE_TENSION = 1.0 / 20.0;  // sigma
+constexpr Float CX              = 1.0;
+constexpr Float CY              = 0.5;
+constexpr Float R0              = 0.25;
+constexpr auto vof0             = [](Float x, Float y) {
+  return static_cast<Float>(Igor::sqr(x - CX) + Igor::sqr(y - CY) <= Igor::sqr(R0));
+};
+
+constexpr int PRESSURE_MAX_ITER = 10;
 constexpr Float PRESSURE_TOL    = 1e-6;
 
 constexpr Index NUM_SUBITER = 5;
@@ -102,23 +110,32 @@ auto main() -> int {
   Matrix<Float, NX, NY> Vi{};
   Matrix<Float, NX, NY> div{};
 
+  Matrix<Float, NX, NY> vof_old{};
   Matrix<Float, NX, NY> vof{};
 
   Matrix<Float, NX + 1, NY> drhoUdt{};
   Matrix<Float, NX, NY + 1> drhoVdt{};
   Matrix<Float, NX, NY> delta_p{};
 
+  // Observation variables
   Float t  = 0.0;
   Float dt = DT_MAX;
 
-  Float U_max         = 0.0;
-  Float V_max         = 0.0;
-  Float div_max       = 0.0;
+  Float U_max = 0.0;
+  Float V_max = 0.0;
+
+  Float div_max = 0.0;
+  Float div_L1  = 0.0;
+
   Float vof_min       = 0.0;
   Float vof_max       = 0.0;
   Float vof_integral  = 0.0;
   Float vof_loss      = 0.0;
   Float vof_loss_prct = 0.0;
+  Float vof_vol_error = 0.0;
+
+  Float pressure_res  = 0.0;
+  Index pressure_iter = 0;
   // = Allocate memory =============================================================================
 
   // = Output ======================================================================================
@@ -133,14 +150,22 @@ auto main() -> int {
   Monitor<Float> monitor(Igor::detail::format("{}/monitor.log", OUTPUT_DIR));
   monitor.add_variable(&t, "time");
   monitor.add_variable(&dt, "dt");
+
   monitor.add_variable(&U_max, "max(U)");
   monitor.add_variable(&V_max, "max(V)");
+
   monitor.add_variable(&div_max, "max(div)");
+  monitor.add_variable(&div_L1, "L1(div)");
+
+  monitor.add_variable(&pressure_res, "res(p)");
+  monitor.add_variable(&pressure_iter, "iter(p)");
+
   monitor.add_variable(&vof_min, "min(vof)");
   monitor.add_variable(&vof_max, "max(vof)");
   monitor.add_variable(&vof_integral, "int(vof)");
   monitor.add_variable(&vof_loss, "loss(vof)");
-  monitor.add_variable(&vof_loss_prct, "loss(vof) [%]");
+  // monitor.add_variable(&vof_loss_prct, "loss(vof) [%]");
+  monitor.add_variable(&vof_vol_error, "max(vol. error)");
   // = Output ======================================================================================
 
   // = Initialize grid =============================================================================
@@ -154,9 +179,6 @@ auto main() -> int {
   // = Initialize grid =============================================================================
 
   // = Initialize VOF field ========================================================================
-  auto vof0 = [](Float x, Float y) {
-    return static_cast<Float>(Igor::sqr(x - 1.0) + Igor::sqr(y - 0.5) <= Igor::sqr(0.25));
-  };
   for (Index i = 0; i < vof.extent(0); ++i) {
     for (Index j = 0; j < vof.extent(1); ++j) {
       vof[i, j] =
@@ -191,6 +213,7 @@ auto main() -> int {
   U_max   = max(fs.U);
   V_max   = max(fs.V);
   div_max = max(div);
+  div_L1  = L1_norm(fs.dx, fs.dy, div) / ((X_MAX - X_MIN) * (Y_MAX - Y_MIN));
   calc_vof_stats(
       fs, vof, init_vof_integral, vof_min, vof_max, vof_integral, vof_loss, vof_loss_prct);
   if (!vtk_writer.write(t)) { return 1; }
@@ -207,13 +230,21 @@ auto main() -> int {
     // Save previous state
     std::copy_n(fs.U.get_data(), fs.U.size(), fs.U_old.get_data());
     std::copy_n(fs.V.get_data(), fs.V.size(), fs.V_old.get_data());
+    std::copy_n(vof.get_data(), vof.size(), vof_old.get_data());
+
+    // = Update VOF field ==========================================================================
+    reconstruct_interface(fs.x, fs.y, vof_old, ir);
+
+    // interpolate_U(fs.U, Ui);
+    // interpolate_V(fs.V, Vi);
+    // advect_cells(fs, vof_old, Ui, Vi, dt, ir, vof, &vof_vol_error);
+    calc_rho_and_visc(vof, fs);
 
     for (Index sub_iter = 0; sub_iter < NUM_SUBITER; ++sub_iter) {
       calc_mid_time(fs.U, fs.U_old);
       calc_mid_time(fs.V, fs.V_old);
 
       // = Update flow field =======================================================================
-      // TODO: Handle density and interfaces
       calc_dmomdt(fs, drhoUdt, drhoVdt);
       for (Index i = 1; i < fs.U.extent(0) - 1; ++i) {
         for (Index j = 1; j < fs.U.extent(1) - 1; ++j) {
@@ -233,15 +264,38 @@ auto main() -> int {
 
       calc_divergence(fs, div);
       // TODO: Add capillary forces here.
-      if (!ps.solve(fs, div, dt, delta_p)) {
-        Igor::Warn("Pressure correction failed at t={}.", t);
-        failed = true;
+      {
+        auto get_curvature = [](Float x, Float y) {
+          return 1.0 / std::sqrt(Igor::sqr(x - CX) + Igor::sqr(y - CY));
+        };
+        for (Index i = 0; i < NX; ++i) {
+          for (Index j = 0; j < NY; ++j) {
+            if (has_interface(vof, i, j)) {
+              const auto dkappadx =
+                  (get_curvature(fs.xm[i + 1], fs.ym[j]) - get_curvature(fs.xm[i - 1], fs.ym[j])) /
+                  (2.0 * fs.dx[i]);
+              const auto dkappady =
+                  (get_curvature(fs.xm[i], fs.ym[j + 1]) - get_curvature(fs.xm[i], fs.ym[j - 1])) /
+                  (2.0 * fs.dy[j]);
+
+              IGOR_ASSERT((ir.interface[i, j].getNumberOfPlanes() == 1),
+                          "Expected exactly one plane but got {}",
+                          ir.interface[i, j].getNumberOfPlanes());
+
+              const IRL::Normal n = ir.interface[i, j][0].normal();
+              IGOR_ASSERT(std::abs(n[2]) < 1e-12,
+                          "Expected z-component of normal to be 0 but is {:.6e}",
+                          n[2]);
+
+              div[i, j] += dt * 2.0 * SURFACE_TENSION * (n[0] * dkappadx + n[1] * dkappady);
+            }
+          }
+        }
       }
 
-      if (std::any_of(delta_p.get_data(), delta_p.get_data() + delta_p.size(), [](Float x) {
-            return std::isnan(x);
-          })) {
-        Igor::Panic("NaN value in delta_p.");
+      if (!ps.solve(fs, div, dt, delta_p, &pressure_res, &pressure_iter)) {
+        Igor::Warn("Pressure correction failed at t={}.", t);
+        failed = true;
       }
 
       shift_pressure_to_zero(fs, delta_p);
@@ -272,9 +326,9 @@ auto main() -> int {
     U_max   = max(fs.U);
     V_max   = max(fs.V);
     div_max = max(div);
+    div_L1  = L1_norm(fs.dx, fs.dy, div) / ((X_MAX - X_MIN) * (Y_MAX - Y_MIN));
     calc_vof_stats(
         fs, vof, init_vof_integral, vof_min, vof_max, vof_integral, vof_loss, vof_loss_prct);
-    // Igor::Todo("Find out why the pressure correction fails.");
     if (should_save(t, dt, DT_WRITE, T_END)) {
       if (!vtk_writer.write(t)) { return 1; }
     }

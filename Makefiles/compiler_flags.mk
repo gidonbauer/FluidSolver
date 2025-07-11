f44f757bdb7524743d066174da4288aaccc91f4b
gidon.bauer <g.bauer@itv.rwth-aachen.de>
2025-07-11 11:35:08 +0200

    Add build option for safe and unsafe optimized builds

--- Makefiles/compiler_flags.mk
+++ Makefiles/compiler_flags.mk
@@ -3,7 +3,8 @@ BASENAME_CXX = ${notdir ${CXX}}
 ifeq (${BASENAME_CXX}, clang++)
 
 	CXX_FLAGS = -Wall -Wextra -pedantic -Wshadow -Wconversion -std=c++23
-	CXX_RELEASE_FLAGS = -march=native -ffast-math -O3 # -DIGOR_NDEBUG
+	CXX_RELEASE_FLAGS = -march=native -O3
+	CXX_FAST_FLAGS = ${CXX_RELEASE_FLAGS} -ffast-math -DIGOR_NDEBUG
 	CXX_DEBUG_FLAGS = -O0 -g
 	CXX_SANITIZER_FLAGS = -fsanitize=address,undefined
 	CXX_OPENMP_FLAGS = -fopenmp
@@ -11,7 +12,8 @@ ifeq (${BASENAME_CXX}, clang++)
 else ifeq (${BASENAME_CXX}, ${filter ${BASENAME_CXX}, g++ g++-15})
 
 	CXX_FLAGS = -Wall -Wextra -pedantic -Wshadow -Wconversion -std=c++23
-	CXX_RELEASE_FLAGS = -march=native -ffast-math -O3 # -DIGOR_NDEBUG
+	CXX_RELEASE_FLAGS = -march=native -O3
+	CXX_FAST_FLAGS = ${CXX_RELEASE_FLAGS} -ffast-math -DIGOR_NDEBUG
 	CXX_DEBUG_FLAGS = -O0 -g
 	CXX_SANITIZER_FLAGS = -fsanitize=address,undefined
 	CXX_OPENMP_FLAGS = -fopenmp
@@ -19,7 +21,8 @@ else ifeq (${BASENAME_CXX}, ${filter ${BASENAME_CXX}, g++ g++-15})
 else ifeq (${BASENAME_CXX}, icpx)
 
 	CXX_FLAGS = -Wall -Wextra -pedantic -Wshadow -Wconversion -std=c++23
-	CXX_RELEASE_FLAGS = -O3 -ffast-math -xSSE4.2 -axCORE-AVX2,AVX -fp-model fast=2 # -DIGOR_NDEBUG
+	CXX_RELEASE_FLAGS = -O3 -xSSE4.2 -axCORE-AVX2,AVX -fp-model precise
+	CXX_FAST_FLAGS = -O3 -xSSE4.2 -axCORE-AVX2,AVX -fp-model fast=2 -ffast-math -DIGOR_NDEBUG
 	CXX_DEBUG_FLAGS = -O0 -g
 	CXX_SANITIZER_FLAGS = -fsanitize=address,leak,undefined
 	CXX_OPENMP_FLAGS = -qopenmp
@@ -30,19 +33,23 @@ else
 
 endif
 
-DEBUG ?= 0
-ifeq (${DEBUG}, 0)
-  CXX_FLAGS += ${CXX_RELEASE_FLAGS}
-else
+DEBUG    ?= 0
+FAST     ?= 0
+SANITIZE ?= 0
+SCOREP   ?= 0
+
+ifeq (${DEBUG}, 1)
   CXX_FLAGS += ${CXX_DEBUG_FLAGS}
+else ifeq (${FAST}, 1)
+  CXX_FLAGS += ${CXX_FAST_FLAGS}
+else
+  CXX_FLAGS += ${CXX_RELEASE_FLAGS}
 endif
 
-SANITIZE ?= 0
 ifeq (${SANITIZE}, 1)
   CXX_FLAGS += ${CXX_SANITIZER_FLAGS}
 endif
 
-SCOREP ?= 0
 ifeq (${SCOREP}, 1)
   CXX_FLAGS += -g
   CXX := scorep ${CXX}

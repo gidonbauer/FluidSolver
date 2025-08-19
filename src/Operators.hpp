faba69809e7f25bca205601175c29c2dd532949e
gidon.bauer <g.bauer@itv.rwth-aachen.de>
2025-08-19 10:32:41 +0200

    Fix Curvature

--- src/Operators.hpp
+++ src/Operators.hpp
@@ -88,11 +88,12 @@ template <typename Float, Index NX, Index NY, Index NGHOST>
                                                   const Matrix<Float, NX, NY, NGHOST>& field,
                                                   Float x,
                                                   Float y) -> Float {
-  const auto dx = xm[1] - xm[0];
-  const auto dy = ym[1] - ym[0];
+  const auto dx    = xm[1] - xm[0];
+  const auto dy    = ym[1] - ym[0];
 
-  auto get_indices =
-      []<Index N>(Float pos, const Vector<Float, N>& grid, Float delta) -> std::pair<Index, Index> {
+  auto get_indices = []<Index N>(Float pos,
+                                 const Vector<Float, N, NGHOST>& grid,
+                                 Float delta) -> std::pair<Index, Index> {
     if (pos <= grid[0]) { return {0, 0}; }
     if (pos >= grid[N - 1]) { return {N - 1, N - 1}; }
     const auto prev = static_cast<Index>(std::floor((pos - grid[0]) / delta));

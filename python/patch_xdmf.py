0debaa49459b11328811f8587ad3ad12b3e4b6fe
gidon.bauer <g.bauer@itv.rwth-aachen.de>
2025-08-26 13:51:40 +0200

    Update patching XDMF files

--- python/patch_xdmf.py
+++ python/patch_xdmf.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 
 import sys
+import os
 
+
+def file_needs_patching(filename: str) -> bool:
+    try:
+        with open(filename, 'rb') as f:
+            try:
+                f.seek(-2, os.SEEK_END)
+                while f.read(1) != b'\n':
+                    f.seek(-2, os.SEEK_CUR)
+            except OSError as err:
+                print(f"{err}")
+                sys.exit(1)
+            last_line = f.readline().decode()
+            return not "</Xdmf>" in last_line
+    except IOError as err:
+        print(f"{err}")
+        sys.exit(1)
 
 def main():
     if len(sys.argv) < 2:
@@ -9,6 +26,9 @@ def main():
         sys.exit(1)
     
     filename = sys.argv[1]
+    if not file_needs_patching(filename):
+        sys.exit(0)
+
     try:
         with open(filename, "a") as f:
             print("    </Grid>", file=f)

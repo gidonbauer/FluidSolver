29f7acbb4e98d345129583cf3822f0cb77a9e536
gidon.bauer <g.bauer@itv.rwth-aachen.de>
2025-10-31 17:06:02 +0100

    Add ClippedNeumann boundary condition

--- src/BoundaryConditions.hpp
+++ src/BoundaryConditions.hpp
@@ -16,11 +16,12 @@ struct FS;
 template <typename Float>
 struct Dirichlet { Float U{}, V{}; };
 struct Neumann {};
+struct ClippedNeumann {};
 struct Periodic {};
 struct Symmetry {};
 
 template <typename Float>
-using BCond_t = std::variant<Dirichlet<Float>, Neumann, Periodic, Symmetry>;
+using BCond_t = std::variant<Dirichlet<Float>, ClippedNeumann, Neumann, Periodic, Symmetry>;
 // clang-format on
 
 template <typename Float>
@@ -56,6 +57,11 @@ void apply_velocity_bconds(FS<Float, NX, NY, NGHOST>& fs, const FlowBConds<Float
                                    [&](Index j) { fs.curr.U(-NGHOST, j) = fs.curr.U(0, j); });
         for_each_a<Exec::Parallel>(fs.y, [&](Index j) { fs.curr.V(-NGHOST, j) = fs.curr.V(0, j); });
       },
+      [&](const ClippedNeumann& /*bcond*/) {
+        for_each_a<Exec::Parallel>(
+            fs.ym, [&](Index j) { fs.curr.U(-NGHOST, j) = std::min(fs.curr.U(0, j), 0.0); });
+        for_each_a<Exec::Parallel>(fs.y, [&](Index j) { fs.curr.V(-NGHOST, j) = fs.curr.V(0, j); });
+      },
       [&](const Periodic& /*bcond*/) {
         for_each_a<Exec::Parallel>(fs.ym,
                                    [&](Index j) { fs.curr.U(-NGHOST, j) = fs.curr.U(NX - 1, j); });
@@ -81,6 +87,12 @@ void apply_velocity_bconds(FS<Float, NX, NY, NGHOST>& fs, const FlowBConds<Float
       [&](const Neumann& /*bcond*/) {
         for_each_a<Exec::Parallel>(fs.ym,
                                    [&](Index j) { fs.curr.U(NX + NGHOST, j) = fs.curr.U(NX, j); });
+        for_each_a<Exec::Parallel>(
+            fs.y, [&](Index j) { fs.curr.V(NX + NGHOST - 1, j) = fs.curr.V(NX - 1, j); });
+      },
+      [&](const ClippedNeumann& /*bcond*/) {
+        for_each_a<Exec::Parallel>(
+            fs.ym, [&](Index j) { fs.curr.U(NX + NGHOST, j) = std::max(fs.curr.U(NX, j), 0.0); });
         for_each_a<Exec::Parallel>(
             fs.y, [&](Index j) { fs.curr.V(NX + NGHOST - 1, j) = fs.curr.V(NX - 1, j); });
       },
@@ -112,6 +124,11 @@ void apply_velocity_bconds(FS<Float, NX, NY, NGHOST>& fs, const FlowBConds<Float
         for_each_a<Exec::Parallel>(fs.xm,
                                    [&](Index i) { fs.curr.V(i, -NGHOST) = fs.curr.V(i, 0); });
       },
+      [&](const ClippedNeumann& /*bcond*/) {
+        for_each_a<Exec::Parallel>(fs.x, [&](Index i) { fs.curr.U(i, -NGHOST) = fs.curr.U(i, 0); });
+        for_each_a<Exec::Parallel>(
+            fs.xm, [&](Index i) { fs.curr.V(i, -NGHOST) = std::min(fs.curr.V(i, 0), 0.0); });
+      },
       [&](const Periodic& /*bcond*/) {
         for_each_a<Exec::Parallel>(fs.x,
                                    [&](Index i) { fs.curr.U(i, -NGHOST) = fs.curr.U(i, NY - 1); });
@@ -139,6 +156,12 @@ void apply_velocity_bconds(FS<Float, NX, NY, NGHOST>& fs, const FlowBConds<Float
             fs.x, [&](Index i) { fs.curr.U(i, NY + NGHOST - 1) = fs.curr.U(i, NY - 1); });
         for_each_a<Exec::Parallel>(fs.xm,
                                    [&](Index i) { fs.curr.V(i, NY + NGHOST) = fs.curr.V(i, NY); });
+      },
+      [&](const ClippedNeumann& /*bcond*/) {
+        for_each_a<Exec::Parallel>(
+            fs.x, [&](Index i) { fs.curr.U(i, NY + NGHOST - 1) = fs.curr.U(i, NY - 1); });
+        for_each_a<Exec::Parallel>(
+            fs.xm, [&](Index i) { fs.curr.V(i, NY + NGHOST) = std::max(fs.curr.V(i, NY), 0.0); });
       },
       [&](const Periodic& /*bcond*/) {
         for_each_a<Exec::Parallel>(
